554f8c0c5465d93f1f435bbab94a804001d19410
Max Foster <supermaximo93@gmail.com>
2013-05-07 22:43:50 +0100

    Removed old code from Attack battle state

--- SuperFantasticSteampunk/BattleStates/Attack.cs
+++ SuperFantasticSteampunk/BattleStates/Attack.cs
@@ -9,15 +9,8 @@ namespace SuperFantasticSteampunk.BattleStates
         #region Instance Fields
         private ThinkAction thinkAction;
         private ScriptRunner scriptRunner;
-        private bool scriptStarted;
         private float scriptStartTime;
         private float scriptStartTimer;
-        private Entity battleEntity;
-        private Animation originalAnimation;
-        private Animation attackAnimation;
-        private bool playAttackAnimation;
-        private float timeTilHurt;
-        private bool alreadyHurt;
         #endregion
 
         #region Constructors
@@ -27,13 +20,8 @@ namespace SuperFantasticSteampunk.BattleStates
             if (thinkAction == null)
                 throw new Exception("ThinkAction cannot be null");
             this.thinkAction = thinkAction;
-            scriptStarted = false;
             scriptStartTime = 0.0f;
             scriptStartTimer = 0.0f;
-            battleEntity = thinkAction.Actor.BattleEntity;
-            playAttackAnimation = false;
-            timeTilHurt = 0.0f;
-            alreadyHurt = false;
         }
         #endregion
 
@@ -48,7 +36,7 @@ namespace SuperFantasticSteampunk.BattleStates
                 script = weapon.Data.Script;
             scriptRunner = new ScriptRunner(script, Battle, thinkAction.Actor, thinkAction.Target);
 
-            AnimationState animationState = battleEntity.AnimationState;
+            AnimationState animationState = thinkAction.Actor.BattleEntity.AnimationState;
             scriptStartTime = animationState.Animation.Duration - (animationState.Time % animationState.Animation.Duration);
         }
 
@@ -65,34 +53,6 @@ namespace SuperFantasticSteampunk.BattleStates
                 scriptRunner.Update(gameTime);
                 if (scriptRunner.IsFinished())
                     Finish();
-            }
-        }
-
-        private void hurt()
-        {
-            if (alreadyHurt)
-                return;
-
-            PartyMember target = Battle.GetPartyBattleLayoutForPartyMember(thinkAction.Target).FirstInPartyMembersList(thinkAction.Target);
-            int damage = target.CalculateDamageTaken(thinkAction.Actor);
-            target.DoDamage(damage);
-            if (playAttackAnimation)
-                playHurtAnimationOnPartyMember(target);
-            Logger.Log(thinkAction.Actor.Data.Name + " did " + damage.ToString() + " damage to " + target.Data.Name);
-            if (!target.Alive)
-                target.Kill(Battle);
-            alreadyHurt = true;
-        }
-
-        private void playHurtAnimationOnPartyMember(PartyMember partyMember)
-        {
-            AnimationState animationState = partyMember.BattleEntity.AnimationState;
-            Animation originalAnimation = animationState.Animation;
-            Animation hurtAnimation = partyMember.BattleEntity.Skeleton.Data.FindAnimation("hurt");
-            if (hurtAnimation != null)
-            {
-                animationState.AddAnimation(hurtAnimation, false);
-                animationState.AddAnimation(originalAnimation, true);
             }
         }
         #endregion
