ab572276c51f85eec1e073cb0e0c4061e5b2e53c
Max Foster <supermaximo93@gmail.com>
2013-05-14 11:03:01 +0100

    Removed unnecessary 'using' includes from Game1

--- SuperFantasticSteampunk/Game1.cs
+++ SuperFantasticSteampunk/Game1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
-using Spine;
 
 namespace SuperFantasticSteampunk
 {
